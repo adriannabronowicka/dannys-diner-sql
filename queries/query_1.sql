676fda4ac93fbf5b65c89b73912133bb4d122534
Adrianna <adrianna.bronowicka@o2.pl>
2025-12-03 21:23:11 +0100

    new file

---
+++ queries/query_1.sql
@@ -0,0 +1,8 @@
+--Total amount each customer spent
+
+SELECT s.customer_id, 
+	TO_CHAR(SUM(m.price),'FM$9999.00') AS total_amount
+FROM sales AS s
+INNER JOIN menu AS m ON s.product_id = m.product_id
+GROUP BY s.customer_id
+ORDER BY total_amount DESC;
